832bdc0c0ce8cbccd6814b4839ab724ec677e746
kareemmmfayed <kareemf910zuku@gmail.com>
2025-05-17 15:31:31 +0300

    Add the GET /employee endpoint.

---
+++ src/main/java/com/megadev/hr/dto/employee/GetEmployeeDTO.java
@@ -0,0 +1,19 @@
+package com.megadev.hr.dto.employee;
+
+import com.megadev.hr.enums.JobType;
+import com.megadev.hr.enums.Position;
+import com.megadev.hr.enums.Type;
+
+import java.math.BigDecimal;
+
+public interface GetEmployeeDTO1 {
+    Long getId();
+    String getName();
+    String getPhoneNumber();
+    String getEmail();
+    Position getPosition();
+    Type getType();
+    JobType getJobType();
+    String getHomeTown();
+    BigDecimal getSalary();
+}
